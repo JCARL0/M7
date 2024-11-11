3815d247e8570934acfa9198ddffc821496b090d
JCARL0 <john.magno@estudiant.fjaverianas.com>
2024-11-11 22:30:38 +0100

    M7

---
+++ UF1/Intro.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>INTRO</title>
+</head>
+
+<body>
+    <?php
+    // echo 10;
+    // echo "<br>";
+    // echo "!Hola, mundo!<br>";
+    // echo true;
+    // echo "<br>";
+    // echo false;
+    
+    //==============================================================================================================
+    // Variables
+    
+    // $nombre = "Maria"; // Variable de tipo string
+    // $edad = 15; //Variable de tipo int
+    // $altura = 1.65; //Variable de tipo float
+    // $estudiante = true; //Variable de tipo bool
+    
+    // echo $altura;
+
+    // Ejemplo 1: Calcular el área de un triángulo
+    // $base = 10;
+    // $altura = 5;
+    // $area = 0.5 * $base * $altura;
+    // echo "El area el triangulo es: $area";
+
+
+    // Ejemplo 2: Convertir grados Celsius a Fahrenheit
+    // $celsius = 30;
+    // $fahrenheit = ($celsius * 9/5) + 32;
+    // echo "La temperatura en grados Fahrenheit es: $fahrenheit";
+    
+    // Ejemplo 3: Calcular el promedio de tres números
+    // $num1 = 10.7;
+    // $num2 = 20;
+    // $num3 = 35;
+    // $promedio = ($num1 + $num2 + $num3) / 3;
+    // echo "El promedio e los tres numeros es $promedio";
+
+    // Ejemplo 4: Comprobar si un número es par o impar
+    // $numero = 5;
+    // if ($numero % 2 ==0){
+    //     echo "El numero $numero es par";
+    // }
+    // else{
+    //     echo "El numero $numero es impar";
+    // }
+    
+    $x = "Hello World!";
+    echo substr($x, 3,10)
+
+
+
+
+
+
+    ?>
+</body>
+
+</html>
